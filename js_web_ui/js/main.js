fc213a055b993fb7ae6dcf81f7d5a9406d1a07d5
djfedos <djfedos@example.com>
2022-05-19 17:47:58 +0300

    Add convenience, add word meaning lookup in online Merriam-Webster

--- js_web_ui/js/main.js
+++ js_web_ui/js/main.js
@@ -1,14 +1,14 @@
 // This web UI is made more of less following this tutorial: https://youtu.be/1iysNUrI3lw
 
-
 const search = document.getElementById('search');
 const matchList = document.getElementById('match-list');
+const icon = document.getElementById("icon");
 
 // Here we send a request our suggestion engine
 const outputSuggestions = async prefix => {
+    prefix = prefix.toLowerCase()
     if (prefix.length === 0) {
-        let suggestions = []
-        outputHTML(suggestions)
+        outputHTML([])
     }
     else {
         let endpoint = 'http://0.0.0.0:18000/get_suggestions/' + prefix;
@@ -20,8 +20,7 @@ const outputSuggestions = async prefix => {
         } else {
             console.log('Fetching error, please consider')
         }
-    }
-        
+    }        
 }
 
 const outputHTML = suggestions => {
@@ -35,6 +34,37 @@ const outputHTML = suggestions => {
     else {
         matchList.innerHTML = ''
     }
+
+    // Add a suggested word to the search box by click on it
+    // Thanks to this video: https://youtu.be/QxMBHi_ZiT8
+    let allSuggs = matchList.querySelectorAll("div");
+
+    for (let i = 0; i < allSuggs.length; i++) {
+        allSuggs[i].setAttribute("onclick", "select(this)");
+    } 
+
 }
+
+function select(element) {
+    let chosenWord = element.textContent;
+    search.value = chosenWord
+    outputHTML([])
+}
+
+// By click on a zoom icon the script looks for a definition of the word (any user input) in Merriam-Webster dictionary
+icon.addEventListener("click", () => {
+    let userRequest = 'https://www.merriam-webster.com/dictionary/' + search.value;
+    window.open(userRequest, '_blank');
+});
+
+// The same thing happens by pressing Enter key in the search bar
+// https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp
+search.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        icon.click();
+        }
+    });
+
 
 search.addEventListener('input', () => outputSuggestions(search.value));
