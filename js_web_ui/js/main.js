e373b6c2f7dab84ffbdbfde9c7a61248e744e2f2
djfedos <djfedos@example.com>
2022-05-20 17:30:05 +0300

    Add focusing on suggestions with mouse and Tab or Shift + Tab

--- js_web_ui/js/main.js
+++ js_web_ui/js/main.js
@@ -6,27 +6,27 @@ const icon = document.getElementById("icon");
 
 // Here we send a request our suggestion engine
 const outputSuggestions = async prefix => {
-    prefix = prefix.toLowerCase()
+    prefix = prefix.toLowerCase();
     if (prefix.length === 0) {
-        outputHTML([])
+        outputHTML([]);
     }
     else {
         let endpoint = 'http://0.0.0.0:18000/get_suggestions/' + prefix;
         let response = await fetch(endpoint);
         if (response.ok) {
             let responseJSON = await response.json();
-            let suggestions = responseJSON['result']
-            outputHTML(suggestions)
+            let suggestions = responseJSON['result'];
+            outputHTML(suggestions);
         } else {
-            console.log('Fetching error, please consider')
+            console.log('Fetching error, please consider');
         }
     }        
 }
 
 const outputHTML = suggestions => {
     if(suggestions.length > 0){
-        const htlm = suggestions.map(suggestion => 
-            `<div class="card card-body">${suggestion}</div>`).join(' ');
+        const htlm = suggestions.map((sugg_value, sugg_index) => 
+            `<div class="card card-body" tabindex="${sugg_index + 1}">${sugg_value}</div>`).join(' ');
     
     matchList.innerHTML = htlm
 
@@ -41,15 +41,36 @@ const outputHTML = suggestions => {
 
     for (let i = 0; i < allSuggs.length; i++) {
         allSuggs[i].setAttribute("onclick", "select(this)");
-    } 
+        allSuggs[i].setAttribute("onmouseover", "this.focus()");
+        allSuggs[i].setAttribute("onmouseout", "this.blur()");
+        allSuggs[i].setAttribute("onkeypress", "search.focus()");
+        allSuggs[i].setAttribute("onfocus", "setFocus(this)");
+    }
 
 }
 
 function select(element) {
     let chosenWord = element.textContent;
-    search.value = chosenWord
-    outputHTML([])
+    search.value = chosenWord;
+    outputHTML([]);
+    search.focus();
 }
+
+function setFocus(element) {
+    let chosenWord = element.textContent;
+    element.addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            search.value = chosenWord;
+            outputHTML([]);
+            search.focus();
+        }
+        if (event.key === "Backspace") {
+            search.focus();
+        }
+    });
+}
+
 
 // By click on a zoom icon the script looks for a definition of the word (any user input) in Merriam-Webster dictionary
 icon.addEventListener("click", () => {
@@ -64,7 +85,9 @@ search.addEventListener("keypress", function(event) {
         event.preventDefault();
         icon.click();
         }
+    
     });
+
 
 
 search.addEventListener('input', () => outputSuggestions(search.value));
